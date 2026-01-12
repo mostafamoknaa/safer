5904c6064428556075e0601ca5cb164b85b6de58
Mostafa <mostafamokna78@gmail.com>
2026-01-12 22:09:19 +0200

    enhnace

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\HotelController;
 use App\Http\Controllers\Admin\HotelRoomController;
 use App\Http\Controllers\Admin\PolicyController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\MasterServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -88,6 +89,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
         // Services routes
         Route::resource('buses', \App\Http\Controllers\Admin\BusController::class);
         Route::resource('trips', \App\Http\Controllers\Admin\TripController::class);
+        Route::resource('master-services', MasterServiceController::class)->except('show');
         Route::resource('private-cars', \App\Http\Controllers\Admin\PrivateCarController::class);
         Route::resource('service-requests', \App\Http\Controllers\Admin\ServiceRequestController::class)->except(['create', 'store']);
 
